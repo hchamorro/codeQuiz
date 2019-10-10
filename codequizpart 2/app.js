ace2aa09cae4d8d7e2d3b64758f52745204b7719
Hannah Chamorro <hannah.chamorro@gmail.com>
2019-10-10 15:09:51 -0500

    more things

--- codequizpart 2/app.js
+++ codequizpart 2/app.js
@@ -1,11 +1,52 @@
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var qImg = document.getElementById("qImg");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var scoreDiv = document.getElementById("scoreContainer");
+
+var finalQuestion = questions.length - 1;
+var currentQuestion = 0;
+var score = 0;
+
+// show a question
+
+function showQuestion() {
+  var q = questions[currentQuestion]; //making the index set at 0 will need loop
+  question.innerHTML = "<p>" + q.question + "</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.textContent = q.choiceD;
+}
+
+function startQuiz() {
+  // hide start button
+  start.style.display = "none";
+  // show questions
+  showQuestion();
+  //change display from none to visible
+  quiz.style.display = "block";
+  // start timeer
+}
+
+// make score and check anser
+
+function checkAnswer(answer) {
+  if (answer === questions[currentQuestion].correct) {
+    score++;
+  } else {
+  }
+  if (currentQuestion < finalQuestion) {
+    currentQuestion++;
+    showQuestion();
+  } else {
+    alert("finished!");
+    console.log(score);
+  }
+}
+
+start.addEventListener("click", startQuiz);
---
+++ codequizpart 2/questions.js
@@ -0,0 +1,42 @@
+let questions = [
+  {
+    question: "How many tablespoons are in a quater cup?",
+    choiceA: "2",
+    choiceB: "4",
+    choiceC: "6",
+    choiceD: "8",
+    correct: "B"
+  },
+  {
+    question: "How many cups are in a gallon?",
+    choiceA: "4",
+    choiceB: "8",
+    choiceC: "12",
+    choiceD: "16",
+    correct: "D"
+  },
+  {
+    question: "How many pints are in a quart",
+    choiceA: "2",
+    choiceB: "3",
+    choiceC: "4",
+    choiceD: "5",
+    correct: "A"
+  },
+  {
+    question: "How many quarts are in a gallon?",
+    choiceA: "2",
+    choiceB: "3",
+    choiceC: "4",
+    choiceD: "5",
+    correct: "C"
+  },
+  {
+    question: "How many cups are in a quart",
+    choiceA: "2",
+    choiceB: "3",
+    choiceC: "4",
+    choiceD: "5",
+    correct: "C"
+  }
+];
